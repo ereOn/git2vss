c18f1b73b3ffc45827bf264abed94450ea95672d
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-07 11:25:32 +0100

    Added pull() implementation

--- bin/git2vss/__init__.py
+++ bin/git2vss/__init__.py
@@ -3,6 +3,7 @@ git2vss utility functions.
 """
 
 import vss
+import tempfile
 
 from error import Git2VSSMissingOptionError, Git2VSSInvalidGitStatusError
 
@@ -21,7 +22,7 @@ def __get_vss_instance(git_repo, repository_path=None, ss_path=None):
 
     return vss.VSS(repository_path=repository_path, ss_path=ss_path)
 
-def __get_vss_project_path(git_repo, vss, vss_project_path=None):
+def __get_vss_project_path(git_repo, vss_project_path=None):
     """
     Get the VSS project path according to the specified parameters.
     """
@@ -36,6 +37,46 @@ def __get_vss_project_path(git_repo, vss, vss_project_path=None):
 
     return vss_project_path
 
+def __rmtree(path):
+    """
+    Recursively delete a given path, even if it has read-only attributes.
+    """
+
+    def force_delete(func, path, exc_info):
+        os.chmod(path, stat.S_IWRITE)
+        func(path)
+
+    shutil.rmtree(path, onerror=force_delete)
+
+def __copytree(src, dst):
+    """
+    Recursively copy a tree from src to dst.
+    """
+
+    for root, dirs, files in os.walk(src):
+        root = os.path.relpath(root, src)
+
+        for dname in dirs:
+            dst_dir = os.path.join(dst, root, dname)
+            if os.path.isdir(dst_dir):
+                pass
+            else:
+                if os.path.isfile(dst_dir):
+                    os.remove(dst_dir)
+
+                os.mkdir(dst_dir)
+
+        for fname in files:
+            if not os.path.splitext(fname)[1] in ('.scc'):
+                src_file = os.path.join(src, root, fname)
+                dst_file = os.path.join(dst, root, fname)
+
+                if os.path.isfile(dst_file):
+                    os.chmod(dst_file, stat.S_IWRITE)
+
+                shutil.copy(src_file, dst_file)
+                os.chmod(dst_file, stat.S_IWRITE)
+
 def pull(git_repo, repository_path=None, vss_project_path=None, ss_path=None):
     """
     Performs a pull on the VSS repository.
@@ -45,3 +86,12 @@ def pull(git_repo, repository_path=None, vss_project_path=None, ss_path=None):
         raise Git2VSSInvalidGitStatusError('Working directory is dirty. Refusing to pull.', git_repo)
 
     vss_repo = __get_vss_instance(git_repo=git_repo, repository_path=repository_path, ss_path=ss_path)
+    vss_project_path = __get_vss_project_path(git_repo, vss_project_path)
+
+    temp_dir = tempfile.mkdtemp()
+
+    try:
+        vss_repo.get(vss_project_path, recursive=True, get_folder=temp_dir, output='error', ignore='all')
+        __copytree(temp_dir, git_repo.working_dir)
+    finally:
+        __rmtree(temp_dir)
