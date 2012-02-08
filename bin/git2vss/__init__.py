0b36453c3e918a43e01ff285a8bb5254a9e32555
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-08 13:30:19 +0100

    Implemented git vss push

--- bin/git2vss/__init__.py
+++ bin/git2vss/__init__.py
@@ -97,6 +97,34 @@ def __copytree(src, dst):
                 shutil.copy(src_file, dst_file)
                 os.chmod(dst_file, stat.S_IWRITE)
 
+def __scan_directory(path):
+    """
+    Generate a list of all files and directories under the specified path. VSS specials files are ignored.
+    """
+
+    r_dirs = []
+    r_files = []
+
+    for root, dirs, files in os.walk(path):
+        for dname in dirs:
+            r_dirs.append(os.path.relpath(os.path.join(root, dname), path))
+        for fname in files:
+            if not fname.endswith('.scc'):
+                r_files.append(os.path.relpath(os.path.join(root, fname), path))
+
+    return r_dirs, r_files
+
+def __diff(old, new):
+    """Compare two lists and return a tuple in the form (only_in_old, in_both, only_in_new)."""
+
+    o = set(old)
+    n = set(new)
+    only_in_old = list(o - n)
+    only_in_new = list(n - o)
+    in_both = list(set.intersection(o, n))
+
+    return (only_in_old, in_both, only_in_new)
+
 def push(git_repo, ref=None, repository_path=None, vss_project_path=None, ss_path=None):
     """
     Performs a push on the VSS repository.
@@ -108,32 +136,62 @@ def push(git_repo, ref=None, repository_path=None, vss_project_path=None, ss_pat
     vss_repo = __get_vss_instance(git_repo=git_repo, repository_path=repository_path, ss_path=ss_path)
     vss_project_path = __get_vss_project_path(git_repo, vss_project_path)
 
-    temp_dir = tempfile.mkdtemp()
+    vss_temp_dir = tempfile.mkdtemp()
 
     try:
-        # Lets export the git repository
-        temp_git_repo_archive = tempfile.TemporaryFile()
-        git_repo.archive(temp_git_repo_archive)
-
-        vss_repo.checkout(vss_project_path, recursive=True, get_folder=temp_dir, output='error')
+        git_temp_dir = tempfile.mkdtemp()
 
         try:
-            # We compare the folders
+            git_repo.git.checkout_index('-f', '-a', '--prefix=' + git_temp_dir + '/')
 
-            # We first commit the updated files
+            vss_repo.checkout(vss_project_path, recursive=True, get_folder=vss_temp_dir, output='error')
 
-            # We checkin the files back
-            vss_repo.checkin(vss_project_path, recursive=True, get_folder=temp_dir, output='error', comment_no_text=True)
-        except Exception, ex:
-            # Something went wrong: we undo the checkout and propagate the exception
-            vss_repo.undo_checkout(vss_project_path, recursive=True, get_folder=temp_dir, output='error')
-            raise ex
+            try:
+                # We compare the folders
+                vss_dirs, vss_files = __scan_directory(vss_temp_dir)
+                git_dirs, git_files = __scan_directory(git_temp_dir)
 
-        # We then add the new files
+                deleted_dirs, current_dirs, new_dirs = __diff(vss_dirs, git_dirs)
+                deleted_files, current_files, new_files = __diff(vss_files, git_files)
 
-        # We finally remove the deleted files
+                # First commit the updated files
+                for fname in current_files:
+                    shutil.copyfile(os.path.join(git_temp_dir, fname), os.path.join(vss_temp_dir, fname))
+
+                # We checkin the files back
+                vss_repo.checkin(vss_project_path, recursive=True, get_folder=vss_temp_dir, output='error', comment_no_text=True)
+
+            except Exception, ex:
+
+                # Something went wrong: we undo the checkout and propagate the exception
+                vss_repo.undo_checkout(vss_project_path, recursive=True, get_folder=vss_temp_dir, output='error')
+                raise ex
+
+            # We remove the deleted folders and files
+
+            for fname in deleted_files:
+                vss_dir = os.path.join(vss_project_path, fname).replace(os.sep, '/')
+                vss_repo.delete(vss_dir)
+
+            for dname in reversed(sorted(deleted_dirs, key=len)):
+                vss_repo.set_current_project(vss_project_path.replace(os.sep, '/'))
+                vss_dir = os.path.join(vss_project_path, dname).replace(os.sep, '/')
+                vss_repo.delete(vss_dir)
+
+            # We then add the new folders and files
+            for dname in sorted(new_dirs, key=len):
+                vss_dir = os.path.join(vss_project_path, dname).replace(os.sep, '/')
+                vss_repo.create(vss_dir, comment_no_text=True)
+
+            for fname in new_files:
+                vss_dir = os.path.join(vss_project_path, os.path.dirname(fname)).replace(os.sep, '/')
+                vss_repo.set_current_project(vss_dir)
+                vss_repo.add(os.path.join(git_temp_dir, fname), comment_no_text=True)
+
+        finally:
+            __rmtree(git_temp_dir)
     finally:
-        __rmtree(temp_dir)
+        __rmtree(vss_temp_dir)
 
 def pull(git_repo, repository_path=None, vss_project_path=None, ss_path=None):
     """
